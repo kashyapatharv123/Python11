0ab171946b17629aa94da3880d31918c17e557d1
Ashwani <aswanibtech@gmail.com>
2019-07-07 12:51:15 +0530

    si added

--- area_of_traingle.py
+++ area_of_traingle.py
@@ -1,5 +1,10 @@
 from math import *
-a,b,c= 20,85,86
+#a,b,c= 20,85,86
+
+a=int(input("enter first side"))
+b=int(input("enter Second side"))
+c=int(input("enter third side"))
+
 
 s= (a+b+c)/2
 print("S=",s)
---
+++ inputdatauser.py
@@ -0,0 +1,22 @@
+'''name=input("enter your name")
+
+age=int(input("enter your age"))
+age=age+10
+print("Name=",name,"age=",age)
+
+
+a=int(input("enter first number"))
+b=int(input("enter Second number"))
+c=a+b
+print("Sum=",c)
+
+option = input("If U want to continue type y")
+print(option)
+print(option[0])
+print(option)'''
+
+option = input("If U want to continue type y")[0]
+print(option)
+
+result=eval(input("enter any mathematical expression"))
+print(result)
